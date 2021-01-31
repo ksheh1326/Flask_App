ed8733ebb71948e18773a1d70595fdfd896f534d
ksheh1326 <ksheh13@gmail.com>
2021-01-31 07:06:11 -0800

    a

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title="about")
+    return render_template('about.html', title="About")
 
 if __name__ == '__main__':
 	app.run(debug=True)
