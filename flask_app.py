f7e42ca4e44d51b8880db803814a31cec3465c03
ksheh1326 <ksheh13@gmail.com>
2021-01-31 08:20:03 -0800

    a

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -8,6 +9,7 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title="About")
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
